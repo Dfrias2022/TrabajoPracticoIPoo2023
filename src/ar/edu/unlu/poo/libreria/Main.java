ebf84cf06c9a9529e7a5f536726ed3fbff9247ed
Dfrias2022 <diegoarielfrias2020@gmail.com>
2023-09-05 10:50:34 -0300

    TPI_125940_POO_2023

--- src/ar/edu/unlu/poo/libreria/Main.java
+++ src/ar/edu/unlu/poo/libreria/Main.java
@@ -3,9 +3,6 @@ package ar.edu.unlu.poo.libreria;
 import java.util.Iterator;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
         Biblioteca condor = new Biblioteca();
         Libro libro1 = new Libro("El principito", "Antoine de Saint-Exupéry", 120, 10, 10);
@@ -15,7 +12,7 @@ public class Main {
         for(Libro l: condor.getLibros()){
             System.out.println(l.getDescripcion());
         }
-
+//Libro con mayor cantidad de paginas
         if (libro1.getPaginas() > libro2.getPaginas()) {
             System.out.printf("El libro \"%s\" tiene mas paginas (%d) que el libro \"%s\" (%d).\n", libro1.getTitulo(), libro1.getPaginas(), libro2.getTitulo(), libro2.getPaginas());
         } else if (libro1.getPaginas() < libro2.getPaginas()) {
@@ -25,12 +22,12 @@ public class Main {
         }
 
         System.out.println("----Prestamos----");
-
+//Simula 10 prestamos
         for(int i = 0; i < 10; ++i) {
             (condor.getLibros().get(0)).prestar();
             System.out.println((condor.getLibros().get(0)).getDescripcion());
         }
-
+//Se intenta prestar un libro que tiene solo 1 ejemplar disponible
         (condor.getLibros().get(1)).prestar();
         System.out.println((condor.getLibros().get(1)).getDescripcion());
         System.out.printf("Se realizaron %d prestamos en total.\n", condor.prestamosTotales());
