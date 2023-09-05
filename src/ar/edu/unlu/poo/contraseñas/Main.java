58e5fdc83e6157bd475c8d1cfaa9ce2cd1dc29ca
Dfrias2022 <diegoarielfrias2020@gmail.com>
2023-09-05 10:13:22 -0300

    TPI_125940_POO_2023

--- src/ar/edu/unlu/poo/contraseñas/Main.java
+++ src/ar/edu/unlu/poo/contraseñas/Main.java
@@ -9,21 +9,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String opcion = "S";
+        int ingreso = 0;
         boolean estado = false;
         List<Integer> debiles = new ArrayList();
         List<Password> paswords = new ArrayList();
-        System.out.println("Ingrese la cantidad de contraseñas a generar: \n");
-        int ingreso = input.nextInt();
+        do {
+            System.out.println("Ingrese la cantidad de contraseñas a generar: ");
+            if(input.hasNextInt()) {
+                ingreso = input.nextInt();
+            }else{
+                System.out.println("Solo valores enteros positivos.");
+                input.nextLine();
+            }
+        }while (ingreso < 1);
         int valor = 0;
-
         for(int i = 0; i < ingreso; ++i) {
             do {
-                System.out.println("Ingrese la longitud de la contraseña[-1 para usar valor por defecto]:\n");
-                valor = input.nextInt();
-            } while(valor < -1);
-
+                System.out.println("Ingrese la longitud de la contraseña[8 para usar valor por defecto]:");
+                if(input.hasNextInt()) {
+                    valor = input.nextInt();
+                }else{
+                    System.out.println("Solo valores numericos mayores o iguales a 8 (Valor por defecto).");
+                    input.nextLine();
+                }
+            } while(valor < 8);
             Password p1;
-            if (valor == -1) {
+            if (valor == 8) {
                 p1 = new Password();
             } else {
                 p1 = new Password(valor);
@@ -38,7 +49,7 @@ public class Main {
         }
         input.nextLine();
         do {
-            System.out.println("Regenerar las contraseñas debiles?[S/N]\n");
+            System.out.println("Regenerar las contraseñas debiles?[S/N].");
             opcion = input.nextLine().toUpperCase();
             if (opcion.equals("S")) {
                 for(Integer elemento:debiles){
@@ -53,9 +64,14 @@ public class Main {
             debiles.removeIf((elem) -> {
                 return ((Password)paswords.get(elem)).nivelContrasenia() != Tipo.DEBIL;
             });
+            if(!debiles.isEmpty()){
+                System.out.println("-AUN QUEDAN PASSWORDS DEBILES-");
+            }else{
+                System.out.println("YA NO QUEDAN PASSWORDS DEBILES");
+            }
         } while(!debiles.isEmpty() && opcion.equals("S"));
 
-        System.out.println("--MOSTRANDO LA LISTA DE PASSWORDS--\n");
+        System.out.println("--MOSTRANDO LA LISTA DE PASSWORDS--");
         for(Password p:paswords){
             if (p.nivelContrasenia() != Tipo.FUERTE) {
                 System.out.printf("<%s> - DEBIL\n", p.getPass());
